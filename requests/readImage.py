0fd29dcb413b312401730ef4f3e86e729121116e
袁炳坤 <yuanbingkun14307@talkweb.com.cn>
2019-06-19 09:13:07 +0800

    upgrade file

--- requests/readImage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#-*-coding:utf-8-*-
-
-from PIL import Image
-
-img = Image.open('D:\\Documents\\Pictures\\timg.jpg')
-img.show()
-
---
+++ zhx/zhx_data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+import MySQLdb
+
+''' 
+pip2 install MySQL-python
+
+1. 获取所有学校
+2. 获取每所学校下的老师、学生信息
+'''
+
+class ZhxDataDown:
+    # conn = None 
+    # cursor = None 
+
+    def __init__(self,host='localhost',name='zhx',pwd='ZHXpass0o0_',dbName='accountdb_cloud_20182',port=13306):
+        self.host = host
+        self.name = name
+        self.pwd = pwd
+        self.dbName = dbName
+        self.port = port
+        self.conn = MySQLdb.connect(host,name,pwd,dbName,charset='utf8')
+        self.cursor = self.conn.cursor()
+
+    def query_shcool(self):
+        print('query scool')
+        try:
+            self.cursor.execute('select count(*) from t_school')
+            data = self.cursor.fetchone()
+            print(data)
+        except expression as identifier:
+            pass
+
+if __name__ == "__main__":
+    obj = ZhxDataDown()
+    obj.query_shcool()
